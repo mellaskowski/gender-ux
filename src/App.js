b35ed67f4f1051e24679f34879ea53ca2b95195c
melissalaskowski <mellaskowski12@gmail.com>
2025-12-11 15:31:49 -0500

    redefine main page for ux-gender project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import GenderEmoji from './Components/GenderEmoji';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <GenderEmoji />
+        <h1>
+          What's your Gender? 
+        </h1>
+          <p>Please select your gender from the options below, or specify your own. </p>
+          <p>Never feel obligated to fill out this field on our or anyone's form. Your gender identity is fully yours.</p>
       </header>
     </div>
   );
